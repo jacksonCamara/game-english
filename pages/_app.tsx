7789a9fceaf7052111afc38895c1f409a25ed6a9
Jackson Camara <jacksonjld@gmail.com>
2022-01-21 12:42:54 -0300

    Configuring global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
-// import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { GlobalStyle } from "../src/styles/globals";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GlobalStyle />
       <Component {...pageProps} />
     </>
   );
---
+++ src/styles/globals.tsx
@@ -0,0 +1,22 @@
+// Corrigi a formatação dentro de createGlobalStyle
+import { createGlobalStyle } from "styled-components";
+
+const styled = { createGlobalStyle };
+
+export const GlobalStyle = styled.createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
