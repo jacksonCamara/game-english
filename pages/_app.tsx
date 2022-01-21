953abc221a19866239423bfc125b27beb678ef0a
Jackson Camara <jacksonjld@gmail.com>
2022-01-21 14:11:43 -0300

    Configuring global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "../src/styles/globals";
+import { theme } from "../src/styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
