c51be98755a4f226905445119f8879e87a2fb1cd
Jackson Camara <jacksonjld@gmail.com>
2022-01-21 14:25:28 -0300

    Configuring theme  style

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 
 const Title = styled.h1`
   font-size: 50px;
-  color: blue;
+  color: ${({ theme }) => theme.colors.primary};
 `;
 
 const Home: NextPage = () => {
